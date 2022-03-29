6d116bc3ce143ce3e87ddc48ba473de0b99f8bb8
Victor Fernandez <vfernandezInnovation>
2022-03-29 09:48:34 +0200

    Ex3 Completado

--- dnd-character.ts
+++ dnd-character.ts
@@ -1,9 +1,42 @@
+import { throws } from "assert";
+
 export class DnDCharacter {
+
+   strength:number;
+   charisma:number;
+   wisdom:number;
+   intelligence:number;
+   constitution:number;
+   dexterity: number;
+   hitpoints:number;
+
+  
+  constructor(){
+    this.strength = DnDCharacter.generateAbilityScore();
+    this.charisma = DnDCharacter.generateAbilityScore();
+    this.wisdom = DnDCharacter.generateAbilityScore();
+    this.intelligence = DnDCharacter.generateAbilityScore();
+    this.constitution = DnDCharacter.generateAbilityScore();
+    this.dexterity = DnDCharacter.generateAbilityScore();
+    this.hitpoints = DnDCharacter.getModifierFor(this.constitution)+10;
+  }
+  
+
   public static generateAbilityScore(): number {
-    throw new Error('Remove this statement and implement this function')
+    let arrayNumeros:number[] = [tiroDado(), tiroDado(), tiroDado(), tiroDado()]
+    delete arrayNumeros[arrayNumeros.indexOf(Math.min(...arrayNumeros))]
+    let total = arrayNumeros.reduce((a, b) => a + b, 0);
+    
+    return total;
+
+
   }
 
   public static getModifierFor(abilityValue: number): number {
-    throw new Error('Remove this statement and implement this function')
+    return Math.floor((abilityValue - 10) / 2)
   }
+ 
 }
+ function tiroDado():number  {
+    return Math.floor(Math.random() * (6 - 1 + 1) + 1)
+  }
